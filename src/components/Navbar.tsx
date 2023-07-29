c61cb012774bf71ecf3762eac8cd6ffa1ee8bd76
PHIDELIST <phidelisoluoch@gmail.com>
2023-07-29 19:17:47 +0300

    navbar wip

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import './Navbar.css'
+function Navbar(){
+return(
+    <div className='nav-items'>
+        <div className='logo'>Logo</div>
+        <ul className='nav-list'>
+        <li>Skills</li>
+        <li>About me</li>
+        <li>Projects</li>
+        <li>Contact</li>
+       </ul>
+    </div>
+)
+}
+export default Navbar;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/navbar";
+import './HomePage.css'
+export default function HomePage() {
+  return (
+    <>
+    <div className="home-container">
+        <div className="nav-bar"> <Navbar /> </div>
+        <div>HomePage</div>
+    </div>
+    </>
+  )
+}
