19d802874e3bf0eb5410fd2e47b2da2fed7f739e
PHIDELIST <phidelisoluoch@gmail.com>
2023-07-31 19:25:18 +0300

    wip

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,25 +23,25 @@ function Navbar() {
         </div>
     <ul className={`nav-list ${showLinks ? 'active' : ''}`}>
         <li>
-          <ScrollLink to='home' spy={true} smooth={true} offset={-70} duration={500}>Home</ScrollLink>
+          <ScrollLink to='home' spy={true} smooth={true} offset={0} duration={500}>Home</ScrollLink>
         </li>
         <li>
-          <ScrollLink to='about' spy={true} smooth={true} offset={-70} duration={500}>About me</ScrollLink>
+          <ScrollLink to='about' spy={true} smooth={true} offset={0} duration={500}>About me</ScrollLink>
         </li>
         <li>
-          <ScrollLink to='services' spy={true} smooth={true} offset={-70} duration={500}>Services</ScrollLink>
+          <ScrollLink to='services' spy={true} smooth={true} offset={0} duration={500}>Services</ScrollLink>
         </li>
         <li>
-          <ScrollLink to='skills' spy={true} smooth={true} offset={-70} duration={500}>Skills</ScrollLink>
+          <ScrollLink to='skills' spy={true} smooth={true} offset={0} duration={500}>Skills</ScrollLink>
         </li>
         <li>
-          <ScrollLink to='education' spy={true} smooth={true} offset={-70} duration={500}>Education</ScrollLink>
+          <ScrollLink to='education' spy={true} smooth={true} offset={0} duration={500}>Education</ScrollLink>
         </li>
         <li>
-          <ScrollLink to='certification' spy={true} smooth={true} offset={-70} duration={500}>Certification</ScrollLink>
+          <ScrollLink to='certification' spy={true} smooth={true} offset={0} duration={500}>Certification</ScrollLink>
         </li>
         <li>
-          <ScrollLink to='contact' spy={true} smooth={true} offset={-70} duration={500}>Contact</ScrollLink>
+          <ScrollLink to='contact' spy={true} smooth={true} offset={0} duration={500}>Contact</ScrollLink>
         </li>
     </ul>
     </div>
