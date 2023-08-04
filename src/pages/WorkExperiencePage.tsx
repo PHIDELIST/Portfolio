c5a5e588a9cc7a1340df175f7a12f9efea7a819a
PHIDELIST <phidelisoluoch@gmail.com>
2023-08-04 14:19:19 +0300

    work in progress

--- src/pages/WorkExperiencePage.tsx
+++ src/pages/WorkExperiencePage.tsx
@@ -3,14 +3,15 @@ function WorkExperiencePage (){
     return (
         <>
         <div className='experience-container'>
+            <h1>WORK EXPERIENCE</h1>
         <div className='The-Jitu'>
-            <h1>Software Developer Attachee</h1>
+            <h3>Software Developer Attachee</h3>
             <p>The Jitu Limited (May 2023 - July 2023)</p>
             <p>Kenya (Hybrid)</p>
             <p>Developed robust web application using ReactJs,ExpressJs, NodeJs,Redux,JavaScript,MSSQL,Azure,AWS,Docker,etc</p>
         </div>
         <div className='CIL'>
-            <h1>Cloud Engineer Trainee</h1>
+            <h3>Cloud Engineer Trainee</h3>
             <p>Cecure Intelligence Limited (Feb 2023 - Apr 2023 )</p>
             <p>United Kingdom (Remote) </p>
             <p>Mordernized Web application to adopt AWS cloud services</p>
